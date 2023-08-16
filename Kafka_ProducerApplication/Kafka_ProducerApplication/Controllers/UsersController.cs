53d20f2575adc3d806b245987aaa1cd74b0f8f20
Mark <hoanguyen09101998@gmail.com>
2023-08-16 22:13:27 +0700

    init project

---
+++ Kafka_ProducerApplication/Kafka_ProducerApplication/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Kafka.Constants;
+using Kafka.Interfaces;
+using Kafka.Messages.UserRegistration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Kafka_ProducerApplication.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+
+	public class UsersController : ControllerBase
+	{
+		private readonly IKafkaProducer<string, RegisterUser> _kafkaProducer;
+		public UsersController(IKafkaProducer<string, RegisterUser> kafkaProducer)
+		{
+			_kafkaProducer = kafkaProducer;
+		}
+		 
+		[HttpPost]
+		[Route("Register")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[SwaggerOperation("Register User", "This endpoint can be used to register a User ,but for demo produces dummy message in Kafka Topic")]
+		public async Task<IActionResult> ProduceMessage(RegisterUser request)
+		{
+			await _kafkaProducer.ProduceAsync(KafkaTopics.RegisterUser, null, request);
+
+			return Ok("User Registration In Progress");
+		}
+	}
+}
